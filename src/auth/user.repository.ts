289aa80cd90d660d3c738851537ef5af83eca07d
Cristiano Gomes <cristianno@ymail.com>
2019-06-19 22:06:07 -0300

    Adicionando encriptação no password

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,7 @@
 import { Repository, EntityRepository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
+import * as bcrypt from 'bcrypt';
 import {
   ConflictException,
   InternalServerErrorException,
@@ -13,17 +14,21 @@ export class UserRepository extends Repository<User> {
 
     const user = new User();
     user.username = username;
-    user.password = password;
+    user.salt = await bcrypt.genSalt();
+    user.password = await this.hashPassword(password, user.salt);
 
     try {
       await user.save();
     } catch (error) {
       if (error.code === '23505') {
-        //usuário duplicado
         throw new ConflictException('Username already exists');
       }
 
       throw new InternalServerErrorException();
     }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
   }
 }
