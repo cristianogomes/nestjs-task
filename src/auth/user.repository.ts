1afc5e4db4dd5ce8aa800ec9ab269dbdd250f6fb
Cristiano Gomes <cristianno@ymail.com>
2019-06-19 21:47:05 -0300

    Adicionando validação de usuário existente

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,10 @@
 import { Repository, EntityRepository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -11,6 +15,15 @@ export class UserRepository extends Repository<User> {
     user.username = username;
     user.password = password;
 
-    await user.save();
+    try {
+      await user.save();
+    } catch (error) {
+      if (error.code === '23505') {
+        //usuário duplicado
+        throw new ConflictException('Username already exists');
+      }
+
+      throw new InternalServerErrorException();
+    }
   }
 }
